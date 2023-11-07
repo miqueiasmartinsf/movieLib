7cb5a768ecfe6f203d2feeed992a4db5e636b3f4
miqueiasmartinsf <miqueias.martinsf@gmail.com>
2023-11-07 10:23:12 -0300

    remove unuse types and context

--- src/context/appContext.ts
+++ src/context/appContext.ts
@@ -1,13 +1,5 @@
 import { createContext } from "react";
 
-//Types
-import { Movie } from "../types/movieType";
-import { Request } from "../types/requestType";
-
-interface IAppContext {
-    ratedMoviesList: Movie[];
-}
-
 const appContext = createContext(null);
 
 export default appContext;
